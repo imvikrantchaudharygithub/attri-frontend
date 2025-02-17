90eded3b0a3887c6bc2e4a515a6dfb665acca761
Himanshu94Ghosh <himanshughosh882603@gmail.com>
2025-02-18 00:11:27 +0530

    myaccount pages changes done

--- src/pages/myaccount.tsx
+++ src/pages/myaccount.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { SetStateAction, useState } from "react";
 import "@/styles/account.css";
+import AccountSideBar from "@/Components/accountsidebar";
 export default function MyAccount() {
   return (
     <section className="account-box">
@@ -9,158 +10,46 @@ export default function MyAccount() {
 			<h1 className="attriheading">My Account</h1>
 			<div className="account-main d-flex padding-tb">
 				<div className="account-left">
-					<div className="account-left-top active hovertime">
-						<span className="account-icon">
-							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M20.6682 21.5997V19.4664C20.6682 18.3348 20.2187 17.2495 19.4186 16.4494C18.6184 15.6492 17.5332 15.1997 16.4016 15.1997H7.86823C6.73664 15.1997 5.6514 15.6492 4.85124 16.4494C4.05108 17.2495 3.60156 18.3348 3.60156 19.4664V21.5997" stroke="#636266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-								<path d="M12.1358 10.9333C14.4922 10.9333 16.4025 9.02308 16.4025 6.66666C16.4025 4.31025 14.4922 2.39999 12.1358 2.39999C9.77939 2.39999 7.86914 4.31025 7.86914 6.66666C7.86914 9.02308 9.77939 10.9333 12.1358 10.9333Z" stroke="#636266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-							</svg>
-						</span>
-						Profile
-						<span className="account-arrow">
-							<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-							</svg>										
-						</span>
-					</div>
-					<ul className="account-list">
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M20.3889 7.75L11.9444 3L3.5 7.75M20.3889 7.75V17.25L11.9444 22M20.3889 7.75L11.9444 12.5M11.9444 22L3.5 17.25V7.75M11.9444 22V12.5M3.5 7.75L11.9444 12.5M7.93335 10.1778L15.9556 5.32222" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>
-								</span>
-								Orders
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime"> 
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M19.9214 13.0556L12.0088 20.8922L4.09622 13.0556C3.57432 12.5477 3.16322 11.9373 2.88882 11.2627C2.61442 10.5882 2.48265 9.86412 2.50183 9.13614C2.521 8.40817 2.6907 7.69205 3.00024 7.03288C3.30977 6.37371 3.75243 5.78577 4.30035 5.30609C4.84827 4.8264 5.48958 4.46536 6.18389 4.2457C6.8782 4.02604 7.61047 3.95252 8.33458 4.02976C9.0587 4.10701 9.75898 4.33335 10.3913 4.69453C11.0237 5.05571 11.5744 5.54391 12.0088 6.12838C12.4451 5.54815 12.9964 5.06422 13.6283 4.70687C14.2602 4.34952 14.9591 4.12645 15.6812 4.05162C16.4033 3.97679 17.1331 4.05181 17.8248 4.27198C18.5166 4.49215 19.1554 4.85274 19.7014 5.33118C20.2474 5.80962 20.6888 6.3956 20.9979 7.05246C21.307 7.70931 21.4772 8.4229 21.4979 9.14857C21.5185 9.87423 21.3891 10.5963 21.1179 11.2697C20.8466 11.9431 20.4393 12.5532 19.9214 13.0619" stroke="#636266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>
-								Wishlist
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="15" height="21" viewBox="0 0 15 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M7.57692 1C3.94591 1 1 3.80401 1 7.25752C1 11.2313 5.38462 17.5285 6.99459 19.7036C7.06142 19.7954 7.14901 19.8701 7.25021 19.9216C7.35142 19.9731 7.46337 20 7.57692 20C7.69048 20 7.80243 19.9731 7.90363 19.9216C8.00484 19.8701 8.09243 19.7954 8.15925 19.7036C9.76923 17.5295 14.1538 11.2345 14.1538 7.25752C14.1538 3.80401 11.2079 1 7.57692 1Z" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-										<path d="M7.57707 9.76923C8.78785 9.76923 9.76938 8.7877 9.76938 7.57692C9.76938 6.36614 8.78785 5.38461 7.57707 5.38461C6.3663 5.38461 5.38477 6.36614 5.38477 7.57692C5.38477 8.7877 6.3663 9.76923 7.57707 9.76923Z" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>
-								Addresses
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M9 15L15 9M10 9.5C10 9.77614 9.77614 10 9.5 10C9.22386 10 9 9.77614 9 9.5C9 9.22386 9.22386 9 9.5 9C9.77614 9 10 9.22386 10 9.5ZM15 14.5C15 14.7761 14.7761 15 14.5 15C14.2239 15 14 14.7761 14 14.5C14 14.2239 14.2239 14 14.5 14C14.7761 14 15 14.2239 15 14.5ZM3 12C3 13.1819 3.23279 14.3522 3.68508 15.4442C4.13738 16.5361 4.80031 17.5282 5.63604 18.364C6.47177 19.1997 7.46392 19.8626 8.55585 20.3149C9.64778 20.7672 10.8181 21 12 21C13.1819 21 14.3522 20.7672 15.4442 20.3149C16.5361 19.8626 17.5282 19.1997 18.364 18.364C19.1997 17.5282 19.8626 16.5361 20.3149 15.4442C20.7672 14.3522 21 13.1819 21 12C21 10.8181 20.7672 9.64778 20.3149 8.55585C19.8626 7.46392 19.1997 6.47177 18.364 5.63604C17.5282 4.80031 16.5361 4.13738 15.4442 3.68508C14.3522 3.23279 13.1819 3 12 3C10.8181 3 9.64778 3.23279 8.55585 3.68508C7.46392 4.13738 6.47177 4.80031 5.63604 5.63604C4.80031 6.47177 4.13738 7.46392 3.68508 8.55585C3.23279 9.64778 3 10.8181 3 12Z" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>
-								Offers & Coupons
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M3 17L9 11L13 15L21 7M21 7H14M21 7V14" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>																				
-								</span>
-								Trending
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M12 17V17.01M12 13.5C11.9816 13.1754 12.0692 12.8536 12.2495 12.583C12.4299 12.3125 12.6933 12.1079 13 12C13.3759 11.8563 13.7132 11.6272 13.9856 11.331C14.2579 11.0347 14.4577 10.6792 14.5693 10.2926C14.6809 9.90597 14.7013 9.49871 14.6287 9.10288C14.5562 8.70705 14.3928 8.33346 14.1513 8.01153C13.9099 7.68959 13.597 7.4281 13.2373 7.24763C12.8776 7.06716 12.4809 6.97265 12.0785 6.97154C11.6761 6.97042 11.2789 7.06273 10.9182 7.2412C10.5576 7.41967 10.2432 7.67942 10 8.00001M3 12C3 13.1819 3.23279 14.3522 3.68508 15.4442C4.13738 16.5361 4.80031 17.5282 5.63604 18.364C6.47177 19.1997 7.46392 19.8626 8.55585 20.3149C9.64778 20.7672 10.8181 21 12 21C13.1819 21 14.3522 20.7672 15.4442 20.3149C16.5361 19.8626 17.5282 19.1997 18.364 18.364C19.1997 17.5282 19.8626 16.5361 20.3149 15.4442C20.7672 14.3522 21 13.1819 21 12C21 10.8181 20.7672 9.64778 20.3149 8.55585C19.8626 7.46392 19.1997 6.47177 18.364 5.63604C17.5282 4.80031 16.5361 4.13738 15.4442 3.68508C14.3522 3.23279 13.1819 3 12 3C10.8181 3 9.64778 3.23279 8.55585 3.68508C7.46392 4.13738 6.47177 4.80031 5.63604 5.63604C4.80031 6.47177 4.13738 7.46392 3.68508 8.55585C3.23279 9.64778 3 10.8181 3 12Z" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>																														
-								</span>
-								Help Center
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-						<li>
-							<Link href='/' className="hovertime">
-								<span className="account-icon">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<mask id="mask0_1083_2142" maskUnits="userSpaceOnUse" x="3" y="3" width="19" height="19">
-										<rect x="3" y="3" width="19" height="19" fill="#636266"/>
-										</mask>
-										<g mask="url(#mask0_1083_2142)">
-										<path d="M9.96608 20.1H6.5883C6.14038 20.1 5.71081 19.922 5.39408 19.6053C5.07735 19.2886 4.89941 18.859 4.89941 18.4111V6.58885C4.89941 6.14093 5.07735 5.71136 5.39408 5.39463C5.71081 5.0779 6.14038 4.89996 6.5883 4.89996H9.96608" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-										<path d="M15.877 16.7221L20.0992 12.4999L15.877 8.27765" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-										<path d="M20.0992 12.4999H9.96582" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-										</g>
-									</svg>																																								
-								</span>
-								Signout
-								<span className="account-arrow">
-									<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M0.999998 1L11 11L1 21" stroke="#636266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>										
-								</span>									
-							</Link>
-						</li>
-					</ul>
+					<AccountSideBar/>
 				</div>
 				<div className="account-right">
-					<div className="account-form">
-						<form>
-							<div className="form-row d-flex">
-								<div className="form-group">
-									<label className="form-label">Name</label>
-									<input type="text" className="form-control" placeholder="Name"></input>
+					<div className="account-form text-center">
+						<div className="user-profile-icon relative">
+							<Image width={150} height={150} className="w-full" src={'/assets/images/product.jpg'} alt=""></Image>
+							<div className="profile-edit-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+									<path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="attrixsheading">Himanshu Ghosh</div>
+						<p>9999999999</p>
+						<div className="user-wallet d-flex">
+							<div className="wallet-card">
+								<div className="wallet-left">
+									<div className="attrixsheading">Rs.7,490</div>
+									<p>Total Balance</p>
 								</div>
-								<div className="form-group">
-									<label className="form-label">Last Name</label>
-									<input type="text" className="form-control" placeholder="Last Name"></input>
-								</div>
-								<div className="form-group">
-									<label className="form-label">Mobile Number</label>
-									<input type="number" className="form-control" placeholder="Mobile Number"></input>
-								</div>
-								<div className="form-group">
-									<label className="form-label">Email</label>
-									<input type="email" className="form-control" placeholder="Email"></input>
+								<div className="wallet-icon">
+									<svg xmlns="http://www.w3.org/2000/svg" id="wallet" x="0" y="0" version="1.1" viewBox="0 0 20 20">
+										<path d="M16 6H3.5v-.5l11-.88v.88H16V4c0-1.1-.891-1.872-1.979-1.717L3.98 3.717C2.891 3.873 2 4.9 2 6v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2zm-1.5 7.006a1.5 1.5 0 1 1 .001-3.001 1.5 1.5 0 0 1-.001 3.001z"></path>
+									</svg>
 								</div>
 							</div>
-						</form>
+							<div className="wallet-card">
+								<div className="wallet-left">
+									<div className="attrixsheading">Rs.7,490</div>
+									<p>Total Balance</p>
+								</div>
+								<div className="wallet-icon">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" id="money">
+										<path d="M16 17c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm0-14c-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6-2.691-6-6-6z"></path>
+										<path d="M16.4 13.2h-.8a2.613 2.613 0 0 1-2.493-1.864 1 1 0 1 1 1.918-.565c.075.253.312.43.575.43h.8a.6.6 0 0 0 0-1.201h-.8C14.166 10 13 8.833 13 7.4s1.166-2.6 2.6-2.6h.8c1.121 0 2.111.714 2.466 1.778a1 1 0 1 1-1.897.633.598.598 0 0 0-.569-.411h-.8a.6.6 0 0 0 0 1.2h.8c1.434 0 2.6 1.167 2.6 2.6s-1.166 2.6-2.6 2.6z"></path>
+										<path d="M16 6c-.271 0-.521-.11-.71-.29-.04-.05-.09-.1-.12-.16a.556.556 0 0 1-.09-.17.672.672 0 0 1-.061-.18C15.01 5.13 15 5.07 15 5c0-.26.109-.52.29-.71.37-.37 1.04-.37 1.42 0 .18.19.29.45.29.71 0 .07-.01.13-.021.2a.606.606 0 0 1-.06.18.578.578 0 0 1-.09.17c-.04.06-.08.11-.12.16-.189.18-.449.29-.709.29zm0 8c-.271 0-.521-.11-.71-.29-.04-.05-.09-.1-.12-.16a.556.556 0 0 1-.09-.17.672.672 0 0 1-.061-.18c-.009-.07-.019-.13-.019-.2 0-.26.109-.52.29-.71.37-.37 1.04-.37 1.42 0 .18.19.29.45.29.71 0 .07-.01.13-.021.2a.606.606 0 0 1-.06.18.578.578 0 0 1-.09.17c-.04.06-.08.11-.12.16-.189.18-.449.29-.709.29zm2 17H2a1 1 0 0 1-1-1v-9c0-.265.105-.52.293-.707C1.527 20.058 3.653 18 6 18c1.944 0 4.452 1.469 5.295 2H16a3.004 3.004 0 0 1 2.955 3.519l7.891-3.288a2.995 2.995 0 0 1 2.818.273A2.993 2.993 0 0 1 31 23a1 1 0 0 1-.496.864l-12 7A1.003 1.003 0 0 1 18 31zM3 29h14.729l11.14-6.498a1.01 1.01 0 0 0-.314-.334.984.984 0 0 0-.939-.091l-9.23 3.846A1.007 1.007 0 0 1 18 26h-8a1 1 0 1 1 0-2h6a1.001 1.001 0 0 0 0-2h-5c-.197 0-.391-.059-.555-.167C9.68 21.323 7.387 20 6 20c-1.09 0-2.347.88-3 1.439V29z"></path>
+									</svg>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
