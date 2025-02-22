382641e59b6c2453ff73e3f12b0c2a0572706b54
Himanshu94Ghosh <himanshughosh882603@gmail.com>
2025-02-23 00:09:00 +0530

    slider issue done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Footer from "@/Components/footer";
 import Header from "@/Components/header";
 import "@/styles/globals.css";
