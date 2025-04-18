edacede25ad70c27b8a99c0cafce18980ce58550
Vikrant Chaudhary <vikrantchaudhary@Vikrants-MacBook-Pro.local>
2025-04-18 10:43:26 +0530

    live api url updated

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from '@/utils/auth';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api.attriindustries.com/api',
   // withCredentials: true,
   xsrfHeaderName: 'X-XSRF-TOKEN',
   xsrfCookieName: 'XSRF-TOKEN',
